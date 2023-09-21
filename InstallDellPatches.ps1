46f8d86ed7f0d4a61334a12f81d17062188bce65
emily <fabemilyr@gmail.com>
2023-09-21 14:18:12 -0500

    combined dell and bitdefender code

--- InstallDellPatches.ps1
+++
@@ -1,7 +0,0 @@
-$filePath = 'C:\Users\TUCCAdmin\Downloads\Dell Lattitude E7450 Patches'
-
-$fileList = GET-ChildItem -Path $filePath
-
-foreach ($file in $fileList) {
-  Start-Process -FilePath $file.FullName -NoNewWindow -Wait
-}
---
+++ StartupCommands.ps1
@@ -0,0 +1,27 @@
+Set-ExecutionPolicy RemoteSigned
+
+try {
+  #install dell patches
+  $filePath = 'C:\Users\TUCCAdmin\Downloads\Dell Lattitude E7450 Patches'
+  $fileList = GET-ChildItem -Path $filePath
+
+  foreach ($file in $fileList) {
+    Start-Process -FilePath $file.FullName -NoNewWindow -Wait
+  }
+
+  #run bit defender virus scan
+  $consolePath = 'C:\Program Files\Bitdefender\Endpoint Security\product.console.exe'
+
+  Start-Process -FilePath $consolePath -NoNewWindow -Wait
+
+  #### create and send scan command to product console
+  $scanCommand = "FileScan.onDemand.RunScanTask custom"
+
+  Add-Type -AssemblyName System.Windows.Forms
+  Start-Sleep -Seconds 1 # Wait for the console to fully open
+  [System.Windows.Forms.SendKeys]::SendWait($scanCommand)
+  [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+}
+catch {
+  Write-Host "There's an error with installing Dell patches or running Bitdefender: $_"
+}
