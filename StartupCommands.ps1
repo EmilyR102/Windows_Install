7f063ab2adce631961c2106464b1745023247b9c
emily <fabemilyr@gmail.com>
2023-09-22 22:39:03 -0500

    added elevated self-deployment

--- StartupCommands.ps1
+++ StartupCommands.ps1
@@ -1,4 +1,12 @@
-Set-ExecutionPolicy RemoteSigned
+# elevated self-deployment: 
+### Check if the script is running with administrator privileges
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # If not running as administrator, relaunch the script with elevated privileges
+    Start-Process powershell.exe -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"" -Verb RunAs
+    exit
+}
+
+# Set-ExecutionPolicy RemoteSigned
 
 try {
   #install dell patches
@@ -18,12 +26,15 @@ try {
   $scanCommand = "FileScan.onDemand.RunScanTask custom"
 
   Add-Type -AssemblyName System.Windows.Forms
-  Start-Sleep -Seconds 1 # Wait for the console to fully open
+  Start-Sleep -Seconds 1 # Wait for console to fully open
   [System.Windows.Forms.SendKeys]::SendWait($scanCommand)
   [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
 }
 catch {
   Write-Host "There's an error with installing Dell patches or running Bitdefender: $_"
 }
+
+# Remove the scheduled task if it was created
+Unregister-ScheduledTask -TaskName "RunScriptElevated" -Confirm:$false
 
 Remove-Item -Path $MyInvocation.MyCommand.Path -Force #self-delete file
