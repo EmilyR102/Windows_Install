413f9d04d43256d17a272c54ab69f6e2b6438fa8
emily <fabemilyr@gmail.com>
2023-10-03 12:15:58 -0500

    updated scan script

--- scripts/Run-Scan.bat
+++ scripts/Run-Scan.bat
@@ -4,13 +4,14 @@ setlocal enabledelayedexpansion
 set "targetProcess=Installer.exe"
 
 :wait_for_target
+cls
 tasklist /fi "imagename eq %targetProcess%" | find /i "%targetProcess%" > nul
 if %errorlevel% equ 0 (
     echo Bitdefender Endpoint Security Installer is still running. Waiting...
     timeout /t 60 /nobreak > nul
     goto :wait_for_target
 ) else (
-    echo Target application has finished running. Running post-run commands...
+    echo Target application has finished running. Running full virus scan...
 
     "C:\Program Files\Bitdefender\Endpoint Security\product.console.exe" /c FileScan.OnDemand.RunScanTask custom scanBootSectors=true scanRegistry=true scanMemory=true smartScan=false scanRootKits=true scanPUA=true
 
